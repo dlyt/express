dbf25a11e248e3a1da5c5ba4865c994b2491377b
dlyt <miaosonglin@daoxila.com>
2017-08-02 17:57:26 +0800

    8.2

--- index.js
+++ index.js
@@ -9,3 +9,10 @@
 'use strict';
 
 module.exports = require('./lib/express');
+const express = require('./lib/express')
+var app = express();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+app.listen(3000);
